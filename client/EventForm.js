5159818b12b886f5b835061c39bf32580c2cf687
Kira Wiley <kaysimone5@gmail.com>
2017-05-08 16:18:08 -0700

    State is updating with values of each input. #1

--- client/EventForm.js
+++ client/EventForm.js
@@ -3,26 +3,31 @@ const store = require('./store')
 
 const EventForm = (props) => {
   const { name, date, time, notes } = props
-  const handleChange = (event) => {
-    store.dispatch({ type: 'INPUT_CHANGED', text: event.target.value})
-  }
   return (
     <div id="form-container">
       <h5>Create an event:</h5>
       <div className="row">
         <div className="input-field col s4">
-          <input placeholder="Event Name" id="event-name-input" type="text" className="validate" onChange={ handleChange }/>
+          <input placeholder="Event Name" id="event-name-input" type="text" className="validate" onChange={(event) => {
+            store.dispatch({ type: 'INPUT_NAME_CHANGED', text: event.target.value})
+          }}/>
         </div>
         <div className="input-field col s4">
-          <input placeholder="M / D / Y" id="date-input" type="text" className="validate" onChange={ handleChange }/>
+          <input placeholder="M / D / Y" id="date-input" type="text" className="validate" onChange={(event) => {
+            store.dispatch({ type: 'INPUT_DATE_CHANGED', text: event.target.value})
+          }}/>
         </div>
         <div className="input-field col s4">
-          <input placeholder="Time" id="time-input" type="text" className="validate" onChange={ handleChange }/>
+          <input placeholder="Time" id="time-input" type="text" className="validate" onChange={(event) => {
+            store.dispatch({ type: 'INPUT_TIME_CHANGED', text: event.target.value})
+          }}/>
         </div>
       </div>
       <div className="row">
         <div className="input-field col s6">
-          <input placeholder="Event Description/Notes" id="event-notes-input" type="text" className="validate" onChange={ handleChange }/>
+          <input placeholder="Event Description/Notes" id="event-notes-input" type="text" className="validate" onChange={(event) => {
+            store.dispatch({ type: 'INPUT_NOTES_CHANGED', text: event.target.value})
+          }}/>
         </div>
         <button id="submit-button" className="btn waves-effect waves-light" type="submit" name="action">
           Submit
--- client/store.js
+++ client/store.js
@@ -11,9 +11,20 @@ function events(state = eventsState, action) {
 }
 
 function form(state = formState, action) {
+  const stateClone = Object.assign({}, state)
   switch (action.type) {
-    case 'INPUT_CHANGED':
-     return action.text
+    case 'INPUT_NAME_CHANGED':
+     stateClone.name = action.text
+     return stateClone
+    case 'INPUT_DATE_CHANGED':
+      stateClone.date = action.text
+      return stateClone
+    case 'INPUT_TIME_CHANGED':
+      stateClone.time = action.text
+      return stateClone
+    case 'INPUT_NOTES_CHANGED':
+      stateClone.notes = action.text
+      return stateClone
     case 'NEW_EVENT':
       return ''
     default: return state
